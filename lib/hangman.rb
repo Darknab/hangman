171737cf535e3392cab2766c3c6ee5271460c9eb
Darknab <darknab@live.fr>
2023-03-09 18:21:27 +0100

    Wrote the main logic that extracts the data needed to be saved

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -14,15 +14,25 @@ def print_result(word, result, incorrect_letters)
 end
 
 class Hangman
+  attr_accessor :wins, :looses, :current
   attr_reader :name
 
-  def initialize(name, wins = 0, loses = 0)
+  def initialize(name, wins = 0, looses = 0, current = {})
     @name = name
     @wins = wins
-    @loses = loses
+    @looses = looses
+    @current = current
   end
 
-  def round(word, result, incorrect_letters)
+  def update_current(word, result, attempt, incorrect_letters)
+    self.current[:word] = word
+    self.current[:result] = result
+    self.current[:attempt] = attempt
+    self.current[:incorrect_letters] = incorrect_letters
+    return current
+  end
+
+  def round(word, result, incorrect_letters, attempt)
     guess = String.new
   
     loop do 
@@ -42,25 +52,39 @@ class Hangman
     end
     
     print_result(word, result, incorrect_letters)
-  
+    self.update_current(word, result, attempt, incorrect_letters)
     return [result, incorrect_letters]
   end
-  
+ 
   def game
     word = get_random_word
     puts "word length is #{word.length}"
     result = Array.new(word.length,"-").join("")
     incorrect_letters = []
-    i = 1
-    for i in 1 ..8 do
-      puts "Round #{i}"
-      answer = round(word, result, incorrect_letters)
+    attempt = 1
+    for attempt in 1 ..8 do
+      puts "Attempt #{attempt}"
+      answer = round(word, result, incorrect_letters, attempt)
       result = answer[0]
       incorrect_letters = answer[1]
-      break if result == word
-      puts "Game over! the secret word was: #{word}" if i == 8
+      if result == word
+        return "win"
+      end
+      if attempt == 8
+        puts "Game over! the secret word was: #{word}" 
+        return "loose"
+      end
+      puts "Please type C to continue or S to save and exit"
+      choice = gets.chomp.downcase
+      case choice
+      when "c"
+        next
+      when "s"
+        puts "saving: #{self.current}"
+        break
+      end
     end
-  end
+  end 
 end
 
 puts "Welcome to Hangman!"
@@ -74,10 +98,19 @@ loop do
   puts "Do you want to start a new game?(yes/no)"
   response = gets.chomp.downcase
   if response == "yes"
-    player.game
+    case player.game
+    when "win"
+      player.wins += 1
+    when "loose"
+      player.looses += 1
+    end
+    puts "Statistics:    Wins: #{player.wins}    Loses: #{player.looses}"
+
   elsif response == "no"
-    puts "All right, see you next time ;)"
+    puts "Thanks for playing Hangman!"
     break
   else puts "Sorry I didn't understand, can you repeat please?"
   end
 end
+
+ 
